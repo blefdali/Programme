db6412b93b5ec135446a179a3123ce3590499480
Badr Lefdali <b.lefdali@Alexsys.solutions>
2024-02-18 22:32:42 +0100

    test.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { Program } from './program.model';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  value = 'World';
+  title = 'Project';
+
+  ProgramData: Program = {
+    title: 'Your Program Title',
+    elements: [
+      {
+        title: 'Element 1',
+        type: 'Type 1',
+        elements: [
+          { title: 'Subelement 1', type: 'Subtype 1' },
+          { title: 'Subelement 2', type: 'Subtype 2' }
+        ]
+      },
+      { title: 'Element 2', type: 'Type 2' }
+    ]
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-
-import { AppComponent } from "./app.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ProgramComponent } from './program/program.component';
+import { ProgramElementComponent } from './program-element/program-element.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ProgramComponent,
+    ProgramElementComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
